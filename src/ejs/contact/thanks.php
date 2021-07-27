7a19eebaa7cb92da1dffb6d7fb63845a33ebbbeb
kito <k.ito@fmc-inc.jp>
2021-07-27 20:15:57 +0900

    no message

---
+++ src/ejs/contact/thanks.php
@@ -0,0 +1,123 @@
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="keywords" content="高輪精機,高輪,油圧機器,空圧機器,流量計,工作機械,測定,複合技術,生産,省力化,自動化">
+  <meta name="description" content="高輪精機はお客様との関係を単なる商品のみの関係で終えるのではなく、お客様の生産現場で抱える多種多様な諸問題に一緒になって考え、そしてご提案し、きめの細かいフォローをしていきます。">
+  <title>高輪精機 | お問い合わせ</title>
+
+  <link rel="alternate" hreflang="ja" href="http://www.takanawaseiki.co.jp/contact/">
+  <link rel="canonical" href="http://www.takanawaseiki.co.jp/contact/">
+
+
+
+    <!-- ogp -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://www.takanawaseiki.co.jp/contact/">
+    <meta property="og:description" content="高輪精機はお客様との関係を単なる商品のみの関係で終えるのではなく、お客様の生産現場で抱える多種多様な諸問題に一緒になって考え、そしてご提案し、きめの細かいフォローをしていきます。">
+    <meta property="og:image" content="http://www.takanawaseiki.co.jp/contact//images/common/ogimage.png">
+    <meta property="og:title" content="高輪精機 | お問い合わせ">
+    <meta property="og:site_name" content="高輪精機">
+    <!-- /ogp -->
+
+
+
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,500,700&display=swap&subset=japanese" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Kanit:500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+
+</head>
+<body class="index">
+
+  <div class="l-wrapper">
+
+    <header id="header" class="l-header">
+
+      <div class="header-top">
+        <h1 class="header-logo">
+          <a href="/"><img src="/images/common/logo.png" srcset="/images/common/logo.png 1x, /images/common/logo@2x.png 2x" alt="東新ホーム株式会社 Toshin Home"></a>
+        </h1>
+
+        <ul class="header-tel">
+          <li class="header-tel__num"><i><img src="/images/header/icon_tel.png" srcset="/images/header/icon_tel.png 1x, /images/header/icon_tel@2x.png 2x" alt=""></i><span class="u-sp-hide">0120-085-104</span></li>
+          <li class="header-tel__info">
+            <p>営業時間　AM 9:00〜PM 18:00</p>
+            <p>休業日　　毎週水曜日</p>
+          </li>
+        </ul>
+      </div>
+      <div class="l-container">
+
+        <nav id="js-menu" class="globalnavi">
+
+           <ul class="gnavi-list">
+             <li><a href="/#menu">メニュー</a></li>
+             <li><a href="/#flow">リフォームの流れ</a></li>
+             <li><a href="/#company">会社情報</a></li>
+             <li><a href="/#access">アクセス</a></li>
+             <li><a href="/#contact">お問い合わせ</a></li>
+          </ul>
+        </nav>
+
+        <div id="js-menuToggle" class="gnav-btn">
+          <div class="gnav-btn__container">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+      </div>
+
+    </header>
+
+
+
+<!-- ここからコンテンツ -->
+
+
+
+  <main class="l-main">
+
+    <section class="sec-content index-company_sec">
+      <div class="l-container">
+        <div class="sec-contact-box mail_sec">
+          <h2 class="sec-title">Contact<span>お問い合わせ</span></h2>
+          <div class="u-ta-c">
+            <p>ああこの度はお問い合わせいただきまして、誠にありがとうございます。<br>お問い合わせいただきました内容を確認後、担当者より改めてご連絡いたします。</p>
+            <a href="/">TOPに戻る</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+</div>
+
+<!-- ここまでコンテンツ -->
+
+
+
+<footer id="footer" class="l-footer">
+  <div class="l-container">
+    <div class="footer-logo">
+      <a href="/"><img src="/images/common/logo.png" srcset="/images/common/logo.png 1x, /images/common/logo@2x.png 2x" alt="東新ホーム株式会社 Toshin Home"></a>
+    </div>
+
+
+    <p class="footer-copy"><small>Copyright 2019 Toshin Home All Rights Reserved.</small></p>
+  </div>
+</footer>
+
+</div>
+<!-- / l-wrapper -->
+
+<script src="//code.jquery.com/jquery-2.2.2.min.js"></script>
+
+<script src="/js/script.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+</body>
+</html>
